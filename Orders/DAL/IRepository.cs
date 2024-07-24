0fe130a48103dbb0ddcb2bb97d7cb7cfc11b6543
santiago12prietoavila <santiagoprietoavila943@gmail.com>
2024-07-23 19:58:15 -0500

    feat(back-end): Add file RepositoryFactory.cs

--- Orders/DAL/IRepository.cs
+++ Orders/DAL/IRepository.cs
@@ -7,23 +7,24 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    internal interface IRepository : IDisposable
+    public interface IRepository : IDisposable
     {
-        //Agregar un anueva entidad a la base de datos
+        //Agregar una nueva entidad a la base da datos
         Task<TEntity> CreateAsync<TEntity>(TEntity toCreate) where TEntity : class;
 
-        // Para eliminar una entidad
+        // para eliminar una entidad
         Task<bool> DeleteAsync<TEntity>(TEntity toDelete) where TEntity : class;
 
-        //Para actualizar una entidad
+        // para actualizar una entidad
         Task<bool> UpdateAsync<TEntity>(TEntity toUpdate) where TEntity : class;
 
-        //Para recuperar una entidad con base en un criterio
+        // para recuperar una entidad con base en un criterio
 
-        Task<TEntity> RetreiveAsync<TEntity>(Expression<Func<TEntity,bool>> criteria) where TEntity : class;
+        Task<TEntity> RetreiveAsync<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class;
 
-        //Para recuperar un conjunto de entidades con base en un criterio de busqueda
+        //para recuperar un conjunto de entidades con base en un criterio de busqueda
 
         Task<List<TEntity>> FilterAsync<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class;
+
     }
 }
